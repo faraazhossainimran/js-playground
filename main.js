655de62075e986f7e1e641670a1cec572d655f7f
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-07-29 03:28:34 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var onionPrice = 30;
+var eggPrice = 10;
+eggPrice *= 4;
+var payCash = 50;
+totalPrice = onionPrice + eggPrice;
+var cashback = payCash - totalPrice;
+var noback = payCash == totalPrice;
+// console.log(cashback);
+
+var firstName = "10";
+var firstName1 = parseInt(firstName);
+var lastName = "Hossain";
+var fullName = firstName + firstName1 + " " + "lastName";
+console.log(fullName);
+
