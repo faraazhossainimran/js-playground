a1b1ef88a894f0531f8bb509546031f40b6ad7f9
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-07-31 20:57:43 +0600

    problem 2 solved

--- problems.js
+++ problems.js
@@ -1,7 +1,42 @@
-var fruits = ['Apple', 'Banana', 'Orange'];
+// Problems 1
+// var fruits = ['Apple', 'Banana', 'Orange'];
 // fruits.pop('Mango');
-var index = fruits.indexOf('Banana');
-var addNew = fruits.splice(2, 1, "Watermelon");
-console.log(addNew);
-console.log(index);
-console.log(fruits);
+// var index = fruits.indexOf('Banana');
+// var addNew = fruits.splice(2, 1, "Watermelon");
+// console.log(addNew);
+// console.log(index);
+// console.log(fruits);
+
+// problems 2
+var aGrade = 80;
+var bGrade = 60;
+var cGrade = 50;
+var dGrade = 40;
+var fGrade = 39;
+
+var myResult = 85;
+var myTom = 66;
+var myJane = 95;
+var myPeter = 56;
+var myJohn = 40;
+
+if(aGrade >= 80){
+  console.log("Imran and Jane got A grade");
+} else {
+    console.log('Imran and Jane are failed')
+}
+ if(bGrade >= 60) {
+ console.log("Tom got B grade");
+} else {
+    console.log('Tom are failed')
+}
+if (cGrade >= 50) {
+    console.log("Peter got C grade");
+} else {
+    console.log('Peter are failed')
+}
+if (dGrade >= 40) {
+    console.log("John got C grade");
+} else {
+    console.log('John are failed')
+}
