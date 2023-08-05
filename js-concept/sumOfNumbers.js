2e077c8c5f67ba069173eff457fb2edf7e13484d
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-08-06 00:45:24 +0600

    multiplicationOfNumbers added

--- js-concept/sumOfNumbers.js
+++ js-concept/sumOfNumbers.js
@@ -1,5 +1,15 @@
-let sum = 0;
-for(let i = 1; i <=7; i++){
+// let sum = 0;
+// for(let i = 1; i <=7; i++){
+//     sum = sum + i;
+//     console.log(i, sum)
+// }
+
+function sumOfNumbers(number){
+    let sum = 0;
+for(let i = 1; i <=number; i++){
     sum = sum + i;
     console.log(i, sum)
 }
+}
+var sumResult = sumOfNumbers(12);
+console.log(sumResult);
