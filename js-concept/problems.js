1a7ea4438120a32f78b7421b87cf6f138a1de7f6
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-08-10 22:24:55 +0600

    problems file updated

--- js-concept/problems.js
+++ js-concept/problems.js
@@ -1,52 +1,62 @@
-// Problem 1
 function cubeNumber(number) {
     if(typeof number !== 'number' ){
-      const message = 'Please add a number'
-      return message;
+      const cubeNumberMessage = 'Please add a number';
+      return cubeNumberMessage;
     } else {
     const cube = number * number * number;
     return cube;
     }
   }
-  const number= "p";
-  const cubRresult = cubeNumber(number);
-  console.log(cubRresult)
 
-
-
-  // problem 4
-  const obj = {
-    street: 10,
-    house: "15A",
-    society: "Earth Perfect"
-  }
-  function findAddress(obj) {
-    if(!!obj){
-      const street = obj.street || "__";
-      const house = obj.house || "__";
-      const society = obj.society || "__";
-      const address = street+ "," + house + "," + society;
-      console.log(address)
-    } else {
-      console.log("add obj")
-    }
-   console.log(obj);
-  }
-  findAddress(obj);
-
-
-  function matchFinder(str1, str2) {
-    if (str1.indexOf(str2) !== -1) {
+function matchFinder(string1, string2) {
+    if(typeof string1 !== 'string' || typeof string2 !== 'string'){
+      const matchFinderMessage = "Plase add two string";
+      return matchFinderMessage;
+    } else if(string1.indexOf(string2) !== -1){
       return true;
     } else {
       return false;
     }
+  }  
+
+function sortMaker(arr) {
+  var sortedElements = [];
+  for (var i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== "number" || arr[i] < 0) {
+          return "Invalid Input"; 
+      }
+      sortedElements.push(arr[i]);
   }
-  
-  // Test
-  var firstString = "Hello, world!";
-  var secondString = "2";
-  
-  var isMatched = matchFinder(firstString, secondString);
-  
-  console.log(isMatched);
+  for (var i = 0; i < sortedElements.length; i++) {
+          if (sortedElements[i] < sortedElements[i + 1]) { 
+              var temp = sortedElements[i];
+              sortedElements[i] = sortedElements[i + 1];
+              sortedElements[i + 1] = temp;
+          } else if (sortedElements[i] === sortedElements[i + 1]) {
+              return "equal";
+          }
+  }
+  return sortedElements;
+}
+
+function findAddress(obj) {
+      const street = obj.street || "__";
+      const house = obj.house || "__";
+      const society = obj.society || "__";
+      const address = street+ "," + house + "," + society;
+      return address;
+  }
+
+
+function canPay(changeArray, totalDue) {
+  if (changeArray.length === 0) {
+      var canPayMessage = "Please add money at first";
+      return canPayMessage; 
+  } else{
+    var totalCost = 0;
+    for (var i = 0; i < changeArray.length; i++) {
+        totalCost += changeArray[i];
+    }
+    return totalCost >= totalDue;
+  }
+}
