b6d9f0049e20c34891b456be57abce170dd53085
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-08-17 02:46:22 +0600

    js-dom-events added

---
+++ js-dom-events/events.js
@@ -0,0 +1,23 @@
+function makeRed () {
+    document.body.style.backgroundColor = 'red';
+
+}
+
+function makeBlue () {
+    document.body.style.backgroundColor = 'blue';
+    
+}
+
+function makeGreen () {
+    document.body.style.backgroundColor = 'green';
+    
+}
+function makeWhite () {
+    document.body.style.backgroundColor = 'white';
+    
+}
+function makeText () {
+    const heading = document.getElementById("heading");
+    console.log("hi")
+    
+}
